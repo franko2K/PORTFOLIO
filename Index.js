25c2219dd1f1e0b608ec58ce49ebb5f7ebb10ba6
franko2K <franciskamau904@gmail.com>
2024-02-22 08:45:22 +0300

    feature sign up working

---
+++ Index.js
@@ -0,0 +1,12 @@
+//onsole.log("i love you");
+//window.alert("i love cars")
+//let username = window.prompt("What's your name?");
+/* let username;
+
+document.getElementById("myButton").onclick = function(){
+
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = "HELLO " + username;
+}
+*/
